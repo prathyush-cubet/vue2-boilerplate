f68f45bf78d1ab308fb9b6b8c89eaabee1bd7884
prathyush-cubet <Prathyush.Lal@opia.com>
2023-09-08 13:01:08 +0530

    bootstrap added

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,22 @@
 <template>
   <div id="app">
-    <p>
-      <router-link to="/products">Go to Products</router-link>
-      <router-link to="/users">Go to Users</router-link>
-    </p>
+
+    <div>
+  <b-navbar toggleable="lg" type="dark" variant="info">
+    <b-navbar-brand href="#">NavBar</b-navbar-brand>
+
+    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+    <b-collapse id="nav-collapse" is-nav>
+      <b-navbar-nav>
+        <router-link to="/products">Go to Peoducts</router-link>
+        <router-link to="/users">Go to Users</router-link>
+      </b-navbar-nav>
+
+    </b-collapse>
+  </b-navbar>
+</div>
+
     <router-view></router-view>
   </div>
 </template>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,14 @@ import App from './App.vue'
 // we import the vue router from our router/index.js file
 import router from './router'
 
+
+import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+Vue.use(BootstrapVue)
+Vue.use(BootstrapVueIcons)
+
+
 Vue.config.productionTip = false
 
 new Vue({
